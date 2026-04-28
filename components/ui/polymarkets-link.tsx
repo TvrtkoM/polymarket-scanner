671948b1b420b31578277a503481494f1023a573
Tvrtko Majstorović <tvrtkomaj@gmail.com>
2026-04-28 18:01:17 +0200

    use Button component

--- components/ui/polymarkets-link.tsx
+++ components/ui/polymarkets-link.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./button";
 
 export function PolymarketsLink({
   slug,
@@ -10,22 +11,24 @@ export function PolymarketsLink({
   className?: string;
 }) {
   return (
-    <Link
-      href={`https://polymarket.com/market/${slug}`}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={cn(
-        "shrink-0 opacity-60 hover:opacity-100 transition-opacity",
-        className
-      )}
-      aria-label="Open on Polymarket"
-    >
-      <Image
-        src="https://polymarket.com/icons/favicon-32x32.png"
-        alt="Polymarket"
-        height={20}
-        width={20}
-      />
-    </Link>
+    <Button variant={"ghost"} size={"icon-sm"} asChild>
+      <Link
+        href={`https://polymarket.com/market/${slug}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={cn(
+          "shrink-0 opacity-60 hover:opacity-100 transition-opacity",
+          className
+        )}
+        aria-label="Open on Polymarket"
+      >
+        <Image
+          src="https://polymarket.com/icons/favicon-32x32.png"
+          alt="Polymarket"
+          height={20}
+          width={20}
+        />
+      </Link>
+    </Button>
   );
 }

492971d7245a9517eb43ea2083f2c828493dc5da
Tvrtko Majstorović <tvrtkomaj@gmail.com>
2026-05-04 18:10:40 +0200

    add react query devtools

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,12 +4,14 @@ import { getQueryClient } from '@/lib/query-client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Provider as JotaiProvider } from 'jotai'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <JotaiProvider>
       <QueryClientProvider client={getQueryClient()}>
         <NuqsAdapter>{children}</NuqsAdapter>
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </JotaiProvider>
   )

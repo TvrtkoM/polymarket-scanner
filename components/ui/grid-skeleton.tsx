9fe3731209cd9af0e28a3f3506a31a4c74733cb9
Tvrtko Majstorović <tvrtkomaj@gmail.com>
2026-05-07 18:33:41 +0200

    update class

--- components/ui/grid-skeleton.tsx
+++ components/ui/grid-skeleton.tsx
@@ -4,7 +4,7 @@ export function GridSkeleton() {
   return (
     <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
       {Array.from({ length: 6 }).map((_, index) => (
-        <Skeleton key={index} className="w-full h-90" />
+        <Skeleton key={index} className="w-full h-90 rounded-2xl" />
       ))}
     </div>
   )
